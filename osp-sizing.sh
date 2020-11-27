d1bf2fb72eb8178114151e29d5364c5576ca8ae4
fabi8483 <fabian.salamanca@rackspace.com>
2020-11-27 16:44:08 -0600

    CPU usage optimization on Openstack assessment script

--- osp-sizing.sh
+++ osp-sizing.sh
@@ -23,17 +23,21 @@ spacedel
 outlabel "## Openstack projects/tenants"
 outlabel "\`\`\`"
 nice -n 19 openstack project list >> $output
+sleep 3
 outlabel "\`\`\`"
 outlabel "\`\`\`"
 nice -n 19 openstack usage list >> $output
+sleep 3
 outlabel "\`\`\`"
 outlabel "## Cloud Instances" 
 outlabel "\`\`\`"
 nice -n 19 openstack server list --all-projects >> $output
+sleep 3
 outlabel "\`\`\`"
 outlabel "## Cinder volumes"
 outlabel "\`\`\`"
 nice -n 19 openstack volume list --all-projects >> $output
+sleep 3
 outlabel "\`\`\`"
 outlabel "## Redes (Neutron)"
 outlabel "\`\`\`"
@@ -42,33 +46,42 @@ for i in $tenants
 do
 	outlabel "### Redes en proyecto: $i"
 	nice -n 19 openstack network list --project $i >> $output
+	sleep 3
 done
 outlabel "\`\`\`"
 outlabel "\`\`\`"
 spacedel
 nice -n 19 openstack subnet list  >> $output
+sleep 3
 outlabel "\`\`\`"
 outlabel "\`\`\`"
 nice -n 19 openstack subnet pool list  >> $output
+sleep 3
 outlabel "\`\`\`"
 outlabel "## Hosts"
 outlabel "\`\`\`"
 nice -n 19 openstack host list >> $output
+sleep 3
 nice -n 19 openstack hypervisor stats show >> $output
+sleep 3
 outlabel "\`\`\`"
 outlabel "\`\`\`"
 hosts=$(nice -n 19 openstack hypervisor list -f value | awk '{print $2}')
 for i in $hosts
 do
 	nice -n 19 openstack hypervisor show $i -f json >> $output
+	sleep 2
 done
 outlabel "\`\`\`"
 vms=$(nice -n 19 openstack server list --all-projects -f value | awk '{print $2}')
+sleep 3
 outlabel "## Detalle de VMs"
 outlabel "\`\`\`"
 for i in $vms
 do
 	nice -n 19 openstack server show $i >> $output
+	sleep 2
 	nice -n 19 nova diagnostics $i >> $output
+	sleep 2
 done
 outlabel "\`\`\`"
